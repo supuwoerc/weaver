e81e4b89ede82641831ec9a8742ea3fe2f7884a0
supuwoerc <zhangzhouou@gmail.com>
2024-05-14 22:55:01 +0800

    feat: 更新密码校验规则

--- pkg/constant/user.go
+++ pkg/constant/user.go
@@ -2,6 +2,6 @@ package constant
 
 const (
 	EmailRegexPattern  = `^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$`
-	PasswdRegexPattern = `^^(?=.*[0-9])(?=.*[a-zA-Z])[0-9A-Za-z~!@#$%^&*._?]{8,15}$`
+	PasswdRegexPattern = `^[a-fA-F0-9]{32}$`
 	PhoneRegexPattern  = `^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$`
 )
