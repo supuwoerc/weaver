c7db5ce63e0528ccc5f757a27b17802a3488226a
supuwoerc <zhangzhouou@gmail.com>
2024-07-17 22:32:50 +0800

    chore: 添加CI/CD

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define the application name and directories
+APP_NAME="main"
+DEPLOY_DIR="var/www/learn-gin-web"
+PID_FILE="$DEPLOY_DIR/$APP_NAME.pid"
+APP_BINARY="$DEPLOY_DIR/$APP_NAME"
+
+# Function to start the application
+start_app() {
+  echo "Starting the new application..."
+  $APP_BINARY &
+  NEW_PID=$!
+  echo $NEW_PID > $PID_FILE
+  echo "New application started with PID $NEW_PID."
+}
+
+# Check if the application is already running
+if [ -f $PID_FILE ]; then
+  OLD_PID=$(cat $PID_FILE)
+  if [ -n "$OLD_PID" ] && kill -0 $OLD_PID 2>/dev/null; then
+    echo "Stopping the old process with PID $OLD_PID..."
+    kill -9 $OLD_PID
+    echo "Old process stopped."
+  else
+    echo "No old process found or process already stopped."
+  fi
+else
+  echo "No PID file found."
+fi
+
+# Start the new application
+start_app
