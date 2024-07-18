a0f6cc1182fc3810c66f689d9a138685672264af
supuwoerc <zhangzhouou@gmail.com>
2024-07-18 11:15:13 +0800

    fix: 修复文件路径问题

--- initialize/i18n.go
+++ initialize/i18n.go
@@ -2,8 +2,10 @@ package initialize
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
+	"os"
 )
 
 func InitI18N() map[string]*i18n.Localizer {
@@ -12,8 +14,12 @@ func InitI18N() map[string]*i18n.Localizer {
 	// 注册一个JSON加载器
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
 	// 加载语言文件
-	bundle.MustLoadMessageFile("./pkg/locale/en.json")
-	bundle.MustLoadMessageFile("./pkg/locale/zh-Hans.json")
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	bundle.MustLoadMessageFile(fmt.Sprintf("%s%s", dir, "/pkg/locale/en.json"))
+	bundle.MustLoadMessageFile(fmt.Sprintf("%s%s", dir, "/pkg/locale/zh-Hans.json"))
 	return map[string]*i18n.Localizer{
 		"cn": i18n.NewLocalizer(bundle, "zh"),
 		"en": i18n.NewLocalizer(bundle, "en"),
