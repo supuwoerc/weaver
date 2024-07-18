613ca728b82f7a65168365963a4d5358a7589112
supuwoerc <zhangzhouou@gmail.com>
2024-07-18 10:16:49 +0800

    fix: 以生产环境启动服务

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ APP_BINARY="$DEPLOY_DIR/$APP_NAME"
 # Function to start the application
 start_app() {
   echo "Starting the new application..."
-  $APP_BINARY &
+  GIN_MODE=release $APP_BINARY &
   NEW_PID=$!
   echo $NEW_PID > $PID_FILE
   echo "New application started with PID $NEW_PID."
