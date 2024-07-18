2c1325f8a8f10dd8a9b8b92196606c18c4fcea23
supuwoerc <zhangzhouou@gmail.com>
2024-07-19 00:24:39 +0800

    refactor: 拆分国际化文件

--- initialize/i18n.go
+++ initialize/i18n.go
@@ -2,26 +2,60 @@ package initialize
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
+
+func loadJsonFiles(dir string) ([]*i18n.Message, error) {
+	m := []*i18n.Message{}
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".json") {
+			fileBytes, readErr := os.ReadFile(path)
+			if readErr != nil {
+				return readErr
+			}
+			var temp = []*i18n.Message{}
+			if readErr = json.Unmarshal(fileBytes, &temp); readErr != nil {
+				return readErr
+			}
+			m = append(m, temp...)
+		}
+		return nil
+	})
+	return m, err
+}
 
 func InitI18N() map[string]*i18n.Localizer {
 	// 创建一个新的Bundle指定默认语言
-	bundle := i18n.NewBundle(language.SimplifiedChinese)
+	bundle := i18n.NewBundle(language.Chinese)
 	// 注册一个JSON加载器
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
 	// 加载语言文件
-	dir, err := os.Getwd()
+	enMessages, err := loadJsonFiles("./pkg/locale/en")
 	if err != nil {
 		panic(err)
 	}
-	bundle.MustLoadMessageFile(fmt.Sprintf("%s%s", dir, "/pkg/locale/en.json"))
-	bundle.MustLoadMessageFile(fmt.Sprintf("%s%s", dir, "/pkg/locale/zh-Hans.json"))
+	err = bundle.AddMessages(language.English, enMessages...)
+	if err != nil {
+		panic(err)
+	}
+	cnMessages, err := loadJsonFiles("./pkg/locale/zh")
+	if err != nil {
+		panic(err)
+	}
+	err = bundle.AddMessages(language.Chinese, cnMessages...)
+	if err != nil {
+		panic(err)
+	}
 	return map[string]*i18n.Localizer{
-		"cn": i18n.NewLocalizer(bundle, "zh"),
-		"en": i18n.NewLocalizer(bundle, "en"),
+		"cn": i18n.NewLocalizer(bundle, language.Chinese.String()),
+		"en": i18n.NewLocalizer(bundle, language.English.String()),
 	}
 }
