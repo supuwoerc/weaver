cb8bbf143954f6d93ec9109f58e054244ae39987
supuwoerc <zhangzhouou@gmail.com>
2024-07-18 18:06:57 +0800

    fix: 修复启动端口配置

--- initialize/server.go
+++ initialize/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gin-web/pkg/global"
+	"github.com/spf13/viper"
 	"net/http"
 	"os/signal"
 	"syscall"
@@ -11,8 +12,8 @@ import (
 )
 
 const (
-	HOST string = "127.0.0.1"
-	PORT int    = 8080
+	host        string = "127.0.0.1"
+	defaultPort int    = 8080
 )
 
 // 创建http服务器
@@ -20,12 +21,16 @@ func InitServer(handle http.Handler) {
 	// 参考地址:https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+	port := viper.GetInt("server.port")
+	if port == 0 {
+		port = defaultPort
+	}
 	server := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", HOST, PORT),
+		Addr:    fmt.Sprintf("%s:%d", host, port),
 		Handler: handle,
 	}
 	go func() {
-		global.Logger.Info(fmt.Sprintf("服务启动，地址:%s\n", fmt.Sprintf("%s:%d", HOST, PORT)))
+		global.Logger.Info(fmt.Sprintf("服务启动，地址:%s\n", fmt.Sprintf("%s:%d", host, port)))
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			global.Logger.Error(fmt.Sprintf("服务启动失败：%s\n", err.Error()))
 			return
