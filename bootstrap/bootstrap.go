2f2c12a38b9bab55d277c49ab9a81e6d5b234a37
supuwoerc <zhangzhouou@gmail.com>
2024-12-31 22:16:51 +0800

    feat: grace http

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"gin-web/initialize"
 	"gin-web/pkg/global"
+	"github.com/spf13/viper"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func Start() {
@@ -12,9 +16,29 @@ func Start() {
 	global.DB = initialize.InitGORM()
 	global.RedisClient = initialize.InitRedis()
 	global.Localizer = initialize.InitI18N()
+	writePid()
 	initialize.InitServer(initialize.InitEngine(initialize.LoggerSyncer))
 }
 
 func Clean() {
 	fmt.Println("关闭服务后的清理...")
+}
+
+func writePid() {
+	path := viper.GetString("system.pid")
+	if strings.TrimSpace(path) == "" {
+		panic("system.pid is empty!")
+	}
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(file)
+	pid := strconv.Itoa(os.Getpid())
+	_, err = file.WriteString(pid)
+	if err != nil {
+		panic(err)
+	}
 }
--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ start_app() {
   echo "$(date '+%Y-%m-%d %H:%M:%S') - 启动新服务..."
   nohup env GIN_MODE=release $APP_BINARY > $LOG_FILE 2>&1 &
   NEW_PID=$!
-  echo $NEW_PID > $PID_FILE
   echo "$(date '+%Y-%m-%d %H:%M:%S') - 服务启动成功: $NEW_PID"
 }
 
@@ -21,24 +20,6 @@ graceful_restart() {
       echo "$(date '+%Y-%m-%d %H:%M:%S') - 优雅重启旧进程 $OLD_PID"
       kill -USR2 $OLD_PID
       echo "$(date '+%Y-%m-%d %H:%M:%S') - 已触发进程 $OLD_PID 优雅重启"
-
-      # 等待新进程启动
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - 等待新进程启动..."
-      TIMEOUT=30  # 最大等待时间（秒）
-      ELAPSED=0
-      while [ $ELAPSED -lt $TIMEOUT ]; do
-        # 检查进程是否已启动
-        NEW_PID=$(ps aux | grep "$APP_NAME" | grep -v grep | awk '{print $2}')
-        if [ -n "$NEW_PID" ]; then
-          echo "$(date '+%Y-%m-%d %H:%M:%S') - 新进程已启动: $NEW_PID"
-          echo $NEW_PID > $PID_FILE
-          echo "$(date '+%Y-%m-%d %H:%M:%S') - 新进程PID已更新到文件: $NEW_PID"
-          return 0  # 成功启动，退出函数
-        fi
-        sleep 1  # 每秒检查一次
-        ELAPSED=$((ELAPSED + 1))
-      done
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - 超过最大等待时间，新进程未启动"
     else
       echo "$(date '+%Y-%m-%d %H:%M:%S') - 未找到旧进程或进程已停止，启动新服务"
       start_app
