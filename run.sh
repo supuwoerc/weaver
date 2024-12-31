3cc8ef368450d7c102be0eea7ef34c35825f2d2d
supuwoerc <zhangzhouou@gmail.com>
2024-12-31 20:41:00 +0800

    feat: script

--- run.sh
+++ run.sh
@@ -9,26 +9,32 @@ LOG_FILE="$DEPLOY_DIR/$APP_NAME.log"
 
 # Function to start the application
 start_app() {
-  echo "Starting the new application..."
+  echo "启动新服务..."
   nohup env GIN_MODE=release $APP_BINARY > $LOG_FILE 2>&1 &
   NEW_PID=$!
   echo $NEW_PID > $PID_FILE
-  echo "New application started with PID $NEW_PID."
+  echo "服务启动成功: $NEW_PID"
 }
 
-# Check if the application is already running
-if [ -f $PID_FILE ]; then
-  OLD_PID=$(cat $PID_FILE)
-  if [ -n "$OLD_PID" ] && kill -0 $OLD_PID 2>/dev/null; then
-    echo "Stopping the old process with PID $OLD_PID..."
-    kill -USR2 $OLD_PID
-    echo "Old process stopped."
+# Function to perform graceful restart
+graceful_restart() {
+  if [ -f $PID_FILE ]; then
+    OLD_PID=$(cat $PID_FILE)
+    if [ -n "$OLD_PID" ] && kill -0 $OLD_PID 2>/dev/null; then
+      echo "优雅重启旧进程 $OLD_PID"
+      kill -USR2 $OLD_PID
+      echo "已触发进程 $OLD_PID 优雅重启"
+      sleep 5
+    else
+      echo "未找到旧进程或进程已停止"
+    fi
   else
-    echo "No old process found or process already stopped."
+    echo "未发现PID文件,启动新服务"
   fi
-else
-  echo "No PID file found."
-fi
+}
+
+# Perform graceful restart if application is running
+graceful_restart
 
 # Start the new application
 start_app
