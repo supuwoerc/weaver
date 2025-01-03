4582c90e1633d1e5af858799f2add8ed672c6a16
supuwoerc <zhangzhouou@gmail.com>
2025-01-03 18:23:27 +0800

    refactor: grace http

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -3,10 +3,6 @@ package bootstrap
 import (
 	"gin-web/initialize"
 	"gin-web/pkg/global"
-	"github.com/spf13/viper"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func Start() {
@@ -15,25 +11,5 @@ func Start() {
 	global.DB = initialize.InitGORM()
 	global.RedisClient = initialize.InitRedis()
 	global.Localizer = initialize.InitI18N()
-	writePid()
 	initialize.InitServer(initialize.InitEngine(initialize.LoggerSyncer))
-}
-
-func writePid() {
-	path := viper.GetString("system.pid")
-	if strings.TrimSpace(path) == "" {
-		panic("system.pid is empty!")
-	}
-	file, err := os.Create(path)
-	if err != nil {
-		panic(err)
-	}
-	defer func(f *os.File) {
-		_ = f.Close()
-	}(file)
-	pid := strconv.Itoa(os.Getpid())
-	_, err = file.WriteString(pid)
-	if err != nil {
-		panic(err)
-	}
 }
--- run.sh
+++ run.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
 APP_NAME="learn_gin_web"
-DEPLOY_DIR="/var/www/learn-gin-web"
-PID_FILE="$DEPLOY_DIR/$APP_NAME.pid"
+DEPLOY_DIR=$(cd "$(dirname "$0")" && pwd)
 APP_BINARY="$DEPLOY_DIR/$APP_NAME"
 LOG_FILE="$DEPLOY_DIR/$APP_NAME.log"
 
-# 获取进程名称
-get_process_name() {
-  ps -p $1 -o comm= 2>/dev/null
+# 标题
+function title() {
+  echo -e "\033[33m$1\033[0m"
 }
 
-start_app() {
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - 启动新服务..."
-  nohup env GIN_MODE=release $APP_BINARY > $LOG_FILE 2>&1 &
-  NEW_PID=$!
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - 服务启动成功: $NEW_PID"
-  echo $NEW_PID > $PID_FILE
+# 错误
+function error() {
+  echo -e "\033[31mError: $1\033[0m" >&2
 }
 
-graceful_restart() {
-  if [ -f $PID_FILE ]; then
-    OLD_PID=$(cat $PID_FILE)
-    if [ -n "$OLD_PID" ]; then
-      # 检查进程是否存在，并且检查进程名称
-      PROCESS_NAME=$(get_process_name $OLD_PID)
-      if [ "$PROCESS_NAME" == "$APP_NAME" ]; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - 优雅重启旧进程 $OLD_PID"
-        kill -USR2 $OLD_PID
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - 已触发进程 $OLD_PID 优雅重启"
-      else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - PID $OLD_PID 不属于应用 $APP_NAME，启动新服务"
-        start_app
-      fi
-    else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - PID 文件无效，启动新服务"
-      start_app
+# 获取程序的PID
+function get_app_pid() {
+    local app="$1"
+    pgrep -f "${app}"
+}
+
+# 检查应用是否有执行权限
+function check_app_permission() {
+    local app="$1"
+    if [ ! -x "${app}" ]; then
+        error "${app} - 缺少执行权限..."
+        exit 1
     fi
-  else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - 未发现PID文件，启动新服务"
-    start_app
-  fi
 }
 
-# 调用优雅重启
-graceful_restart
+# 启动新的程序
+function start_app() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - 启动新服务..."
+    nohup env GIN_MODE=release $APP_BINARY > $LOG_FILE 2>&1 &
+    NEW_PID=$!
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - 服务启动成功: $NEW_PID"
+}
+
+# 使用grace http重启程序
+function graceful_restart_app() {
+    local pid="$1"
+    kill -USR2 "${pid}"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - 已发送重启信号到 ${pid}"
+}
+
+# 部署程序
+deploy() {
+    # 检查能不能执行程序
+    check_app_permission $APP_BINARY
+    # 获取当前正在运行的程序对应的PID
+    # shellcheck disable=SC2155
+    local pid=$(get_app_pid $APP_BINARY)
+    if [ -n "${pid}" ]; then
+        graceful_restart_app "${pid}"
+    else
+        start_app
+    fi
+}
+
+deploy
