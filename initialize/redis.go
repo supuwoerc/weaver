99048f7edd95ed34c6f2216281224ab0146dc88c
supuwoerc <zhangzhouou@gmail.com>
2025-01-11 17:42:10 +0800

    refactor: redis lock

--- initialize/redis.go
+++ initialize/redis.go
@@ -19,12 +19,12 @@ type RedisLogger struct {
 
 func (r *RedisLogger) DialHook(next goredislib.DialHook) goredislib.DialHook {
 	return func(ctx context.Context, network, addr string) (net.Conn, error) {
-		_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Dialing to Redis at %s://%s\n", time.Now().Format(time.DateTime), network, addr))
+		_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Dialing to Redis at %s://%s\n", time.Now().Format(time.DateTime), network, addr))
 		conn, err := next(ctx, network, addr)
 		if err != nil {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Error dialing Redis: %s\n", time.Now().Format(time.DateTime), err.Error()))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Error dialing Redis: %s\n", time.Now().Format(time.DateTime), err.Error()))
 		} else {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Successfully connected to Redis at %s://%s\n", time.Now().Format(time.DateTime), network, addr))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Successfully connected to Redis at %s://%s\n", time.Now().Format(time.DateTime), network, addr))
 		}
 		return conn, err
 	}
@@ -32,12 +32,12 @@ func (r *RedisLogger) DialHook(next goredislib.DialHook) goredislib.DialHook {
 
 func (r *RedisLogger) ProcessHook(next goredislib.ProcessHook) goredislib.ProcessHook {
 	return func(ctx context.Context, cmd goredislib.Cmder) error {
-		_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Preparing to execute command: %s, Args: %s\n", time.Now().Format(time.DateTime), cmd.Name(), cmd.Args()))
+		_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Preparing to execute command: %s, Args: %s\n", time.Now().Format(time.DateTime), cmd.Name(), cmd.Args()))
 		err := next(ctx, cmd)
 		if err != nil {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Error executing command %s: %s\n", cmd.Name(), time.Now().Format(time.DateTime), err.Error()))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Error executing command %s: %s\n", time.Now().Format(time.DateTime), cmd.Name(), err.Error()))
 		} else {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Successfully executed command: %s\n", time.Now().Format(time.DateTime), cmd.Name()))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Successfully executed command: %s\n", time.Now().Format(time.DateTime), cmd.Name()))
 		}
 		return err
 	}
@@ -46,13 +46,13 @@ func (r *RedisLogger) ProcessHook(next goredislib.ProcessHook) goredislib.Proces
 func (r *RedisLogger) ProcessPipelineHook(next goredislib.ProcessPipelineHook) goredislib.ProcessPipelineHook {
 	return func(ctx context.Context, cmds []goredislib.Cmder) error {
 		for _, cmd := range cmds {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Preparing to execute command in pipeline: %s, Args: %s\n", time.Now().Format(time.DateTime), cmd.Name(), cmd.Args()))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Preparing to execute command in pipeline: %s, Args: %s\n", time.Now().Format(time.DateTime), cmd.Name(), cmd.Args()))
 		}
 		err := next(ctx, cmds)
 		if err != nil {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Error executing commands in pipeline: %s\n", time.Now().Format(time.DateTime), err.Error()))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Error executing commands in pipeline: %s\n", time.Now().Format(time.DateTime), err.Error()))
 		} else {
-			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] %s Successfully executed commands in pipeline", time.Now().Format(time.DateTime)))
+			_, _ = fmt.Fprint(r.logger, fmt.Sprintf("[Redis] [%s] Successfully executed commands in pipeline", time.Now().Format(time.DateTime)))
 		}
 		return err
 	}
--- pkg/utils/lock.go
+++ pkg/utils/lock.go
@@ -9,19 +9,16 @@ import (
 	"gin-web/pkg/response"
 	"github.com/go-redsync/redsync/v4"
 	"github.com/spf13/viper"
-	"sync"
 	"time"
 )
 
 type RedisLock struct {
 	*redsync.Mutex
 	duration time.Duration
-	dog      *Watchdog
-	once     sync.Once
 }
 
 const (
-	defaultMaxRetries = 3
+	defaultMaxRetries = 16
 )
 
 // NewRedisLock 创建锁
@@ -32,7 +29,7 @@ func NewRedisLock(name string, t time.Duration) *RedisLock {
 	}
 }
 
-// Lock 会多次尝试(defaultMaxRetries 次),如果尝试次数内还未获取到锁则返回错误
+// Lock 会多次尝试(defaultMaxRetries 次), 如果尝试次数内还未获取到锁则返回错误
 func Lock(ctx context.Context, lock *RedisLock, extend bool) error {
 	err := lock.Lock()
 	if err != nil {
@@ -64,57 +61,45 @@ func TryLock(ctx context.Context, lock *RedisLock, extend bool) error {
 	return nil
 }
 
+// Unlock 解锁
 func Unlock(lock *RedisLock) error {
-	until := lock.Until()
-	if lock.dog.stopChan == nil || until.IsZero() || until.Before(time.Now()) {
-		return nil
-	}
 	ok, err := lock.Unlock()
 	if err != nil {
+		if errors.Is(err, redsync.ErrLockAlreadyExpired) {
+			return nil
+		}
 		return err
 	}
 	if !ok {
 		return fmt.Errorf("%s unlock failed", lock.Name())
 	}
-	if lock.dog != nil {
-		lock.dog.Stop()
-	}
 	return nil
 }
 
+// autoExtend 自动延长锁的过期时间
 func autoExtend(ctx context.Context, lock *RedisLock) {
-	lock.once.Do(func() {
-		lock.dog = NewWatchdog(lock.duration/2, 0, func() error {
-			if ok, temp := lock.Extend(); temp != nil {
-				return temp
+	ticker := time.NewTicker(lock.duration / 2)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			err := Unlock(lock)
+			if err != nil {
+				go func() {
+					adminEmail := viper.GetString("system.admin.email")
+					if e := email.SendText(adminEmail, "Unlock Fail", fmt.Sprintf("%s unlock fail: %v", lock.Name(), err)); e != nil {
+						global.Logger.Errorf("发送邮件失败,信息:%s", e.Error())
+					}
+				}()
+			}
+			return
+		case <-ticker.C:
+			ok, err := lock.Extend()
+			if err != nil {
+				global.Logger.Errorf("%s extend failed: %v", lock.Name(), err)
 			} else if !ok {
-				return fmt.Errorf("%s extend failed", lock.Name())
-			} else {
-				return nil
+				global.Logger.Errorf("%s extend failed: lock couldn't be extended", lock.Name())
 			}
-		}, func(dog *Watchdog, err error) {
-			go func() {
-				adminEmail := viper.GetString("system.admin.email")
-				if e := email.SendText(adminEmail, "Extend Lock Fail", fmt.Sprintf("%s extend lock fail: %v", lock.Name(), err)); e != nil {
-					global.Logger.Errorf("发送邮件失败,信息:%s", e.Error())
-				}
-			}()
-			dog.Stop()
-		})
-		go func() {
-			select {
-			case <-ctx.Done():
-				err := Unlock(lock)
-				if err != nil {
-					go func() {
-						adminEmail := viper.GetString("system.admin.email")
-						if e := email.SendText(adminEmail, "Unlock Fail", fmt.Sprintf("%s unlock fail: %v", lock.Name(), err)); e != nil {
-							global.Logger.Errorf("发送邮件失败,信息:%s", e.Error())
-						}
-					}()
-				}
-			}
-		}()
-		lock.dog.Start()
-	})
+		}
+	}
 }
