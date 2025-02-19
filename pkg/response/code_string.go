23655475d67d43d35a594c895d109447779df7b6
supuwoerc <zhangzhouou@gmail.com>
2025-02-19 23:56:13 +0800

    refactor: vet

--- pkg/response/code_string.go
+++ pkg/response/code_string.go
@@ -24,7 +24,8 @@ func _() {
 	_ = x[UserLoginFail-20001]
 	_ = x[UserLoginTokenPairCacheErr-20002]
 	_ = x[PasswordValidErr-20003]
-	_ = x[UserNotExist-20004]
+	_ = x[EmailValidErr-20004]
+	_ = x[UserNotExist-20005]
 	_ = x[CaptchaVerifyFail-30000]
 	_ = x[RoleCreateDuplicateName-40000]
 	_ = x[NoValidRoles-40001]
@@ -42,7 +43,7 @@ func _() {
 
 const (
 	_StatusCode_name_0 = "okerrorinvalidParamsinvalidTokencancelRequestrecoveryErrorinvalidRefreshTokenunnecessaryRefreshTokenauthErrnoAuthoritytimeoutErrbusy"
-	_StatusCode_name_1 = "userCreateDuplicateEmailuserLoginFailuserLoginTokenPairCacheErrpasswordValidErruserNotExist"
+	_StatusCode_name_1 = "userCreateDuplicateEmailuserLoginFailuserLoginTokenPairCacheErrpasswordValidErremailValidErruserNotExist"
 	_StatusCode_name_2 = "captchaVerifyFail"
 	_StatusCode_name_3 = "roleCreateDuplicateNamenoValidRolesroleNotExistroleExistPermissionRefroleExistUserRef"
 	_StatusCode_name_4 = "permissionCreateDuplicatepermissionNotExistpermissionExistRoleRef"
@@ -51,7 +52,7 @@ const (
 
 var (
 	_StatusCode_index_0 = [...]uint8{0, 2, 7, 20, 32, 45, 58, 77, 100, 107, 118, 128, 132}
-	_StatusCode_index_1 = [...]uint8{0, 24, 37, 63, 79, 91}
+	_StatusCode_index_1 = [...]uint8{0, 24, 37, 63, 79, 92, 104}
 	_StatusCode_index_3 = [...]uint8{0, 23, 35, 47, 69, 85}
 	_StatusCode_index_4 = [...]uint8{0, 25, 43, 65}
 	_StatusCode_index_5 = [...]uint8{0, 19, 31, 47}
@@ -62,7 +63,7 @@ func (i StatusCode) String() string {
 	case 10000 <= i && i <= 10011:
 		i -= 10000
 		return _StatusCode_name_0[_StatusCode_index_0[i]:_StatusCode_index_0[i+1]]
-	case 20000 <= i && i <= 20004:
+	case 20000 <= i && i <= 20005:
 		i -= 20000
 		return _StatusCode_name_1[_StatusCode_index_1[i]:_StatusCode_index_1[i+1]]
 	case i == 30000:
